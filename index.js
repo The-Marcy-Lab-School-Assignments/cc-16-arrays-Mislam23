b3e77703d7e304c39ae8bfa880185da8e1cba3dc
Mohammad Islam <islam.mohammad.student@gmail.com>
2021-11-16 16:22:50 +0000

    fixed curly braces on my original code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //write your code here
-/*
+
 const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
 
 function replaceWithYerr(array, startIndex, deleteCount) {
@@ -12,26 +12,28 @@ function replaceWithYerr(array, startIndex, deleteCount) {
     } else if (deleteCount === undefined) {
         for (let j = startIndex; j < newArray.length; j++) {
           newArray[j] = "yerr";
+    } 
     } else {
       //  for  (let k = startIndex; k < newArray[deleteCount]; k++) {
         for  (let k = startIndex; k < deleteCount + startIndex; k++) {
           newArray[k] = "yerr"
+        } 
     }
     return newArray;
 }
-*/
 
-const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
 
-function replaceWithYerr(array, startIndex = 0, deleteCount = array.length) {
-    let newArray = [...array]
-    // making a copy
+// const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+
+// function replaceWithYerr(array, startIndex = 0, deleteCount = array.length) {
+//     let newArray = [...array]
+//     // making a copy
    
-      //  for  (let k = startIndex; k < newArray[deleteCount]; k++)
-        for  (let k = startIndex; k < deleteCount + startIndex; k++) {
-          newArray[k] = "yerr"
-    }
-    return newArray;
-}
+//       //  for  (let k = startIndex; k < newArray[deleteCount]; k++)
+//         for  (let k = startIndex; k < deleteCount + startIndex; k++) {
+//           newArray[k] = "yerr"
+//     }
+//     return newArray;
+// }
 
 console.log(replaceWithYerr(months, 1, 3)) //['Jan', 'yerr', 'yerr, 'yerr', 'May', 'June']
