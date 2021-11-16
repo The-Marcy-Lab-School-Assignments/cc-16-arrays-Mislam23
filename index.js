6bdbc9ff8e51b6b33012fd093b0e61f7e0fb2dcb
Mohammad Islam <islam.mohammad.student@gmail.com>
2021-11-16 15:45:39 +0000

    fist of five, 3.5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //write your code here
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+
+function replaceWithYerr(array, startIndex, deleteCount) {
+    let newArray = [...array]
+    // making a copy
+    if (startIndex === undefined && deleteCount === undefined) {
+        for (let i = 0; i < array.length; i++) {
+          newArray[i] = "yerr";
+        }
+    } else if (deleteCount === undefined) {
+        for (let i = startIndex; startIndex < array.length; startIndex++) {
+          newArray[startIndex] = "yerr";
+    }     
+    return newArray;
+}
+
+console.log(replaceWithYerr(months, 1)) //['Jan', 'yerr', 'yerr, 'yerr', 'May', 'June']
